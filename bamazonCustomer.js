d1659765fbc3fd052abe456bc014bcea9b087ffd
Raiicon <orankai@gmail.com>
2018-12-14 16:04:30 -0500

    mostly finished except need to display shoppingCart at checkout

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,6 +7,11 @@ var recordKeeping = [];
 
 var databaseUpdate = [];
 
+var shoppingCart = new Table({
+    head: ['Item Id', 'Item Name', 'Price', 'Quantity'],
+    colWidths: [25, 30, 25, 15]
+});
+
 var total = 0;
 
 const connection = mysql.createConnection({
@@ -31,8 +36,8 @@ function itemForSale() {
         if (err) throw err;
 
         var table = new Table({
-            head: ['Item Id', 'Item Name', 'Price', 'Quantity']
-            , colWidths: [25, 30, 25, 15]
+            head: ['Item Id', 'Item Name', 'Price', 'Quantity'],
+            colWidths: [25, 30, 25, 15]
         });
 
         // table is an Array, so you can `push`, `unshift`, `splice` and friends
@@ -47,59 +52,90 @@ function itemForSale() {
         console.log(table.toString());
         buyItem();
     });
-    
+
 
 };
 //The function to handle the purchasing of items from our database
 function buyItem() {
+
+    
+
     //inquirer will find out what item the customer wants to purchase and how many.
     inquirer.prompt([
         {
             type: 'input',
             name: 'item',
             message: "'Which item would you like to purchase?'"
-        }, 
+        },
         {
             type: 'input',
             message: "How many would you like to purchase?",
             name: 'quantity'
         }
-    ]).then(function(response){
+    ]).then(function (response) {
         // query our database
         const query = "SELECT * FROM products where item_id = ?";
-        connection.query(query, response.item, function(err,res){
-            if (res.length > 0){
+        connection.query(query, response.item, function (err, res) {
+            if (res.length > 0) {
                 //check if there is enough quantity of product in stock
-                if(response.quantity > res[0].stock_quantity){
+                if (response.quantity > res[0].stock_quantity) {
                     console.log('\r\n');
                     console.log("We are unable to fufill your request at this time, please check back later.");
-                    setTimeout(function(){buyItem()}, 2000);
-                    
+                    setTimeout(function () { buyItem() }, 2000);
+
                 } else {
                     recordKeeping.push(res, parseInt(response.quantity));
                     databaseUpdate.push(res[0].item_id, (parseInt(res[0].stock_quantity) - parseInt(response.quantity)));
                     console.log('\r\n');
-                    for (i=0;i<recordKeeping.length;i+=2){
-                        total += (recordKeeping[i][0].price * recordKeeping[i+1]);
+                    for (i = 0; i < recordKeeping.length; i += 2) {
+                        total += (recordKeeping[i][0].price * recordKeeping[i + 1]);
                     }
                     checkOut();
-                }                
+                }
             }
             else {
                 console.log('\r\n');
                 console.log('This item is not in our inventory.');
-                setTimeout(function(){buyItem()},2000);
+                setTimeout(function () { buyItem() }, 2000);
             }
         })
-        let buyQuantity =  parseInt(response.quantity);
+        let buyQuantity = parseInt(response.quantity);
 
-        console.log(stock);
+        // console.log(stock);
 
     });
 };
 
-function checkOut () {
-    
-}
+function checkOut() {
+    console.log(recordKeeping);
+
+    inquirer.prompt({
+        name: 'choice',
+        message: 'What would you like to do now?',
+        type: 'list',
+        choices: ['Continue-Shopping', 'Proceed-to-Checkout', 'quit']
+    }).then(function (response) {
+        let run = true;
+
+        switch (response.choice) {
+            case 'Continue-Shopping':
+                itemForSale();
+                break;
+            case 'Proceed-to-Checkout':
+                for (i = 0; i < databaseUpdate.length; i++) {
+                    query2 = 'UPDATE products SET stock_quantity=? WHERE item_id=?';
+                    connection.query(query2, [databaseUpdate[i+1], databaseUpdate[i]], function(err,res){})
+                }
+                console.log("Purchase processed, thank you!");
+                run = false;
+                break;
+            case 'quit':
+                run = false;
+                break;
+        }
+        if (run){ purchase()};
+    });
+
+};
 
 
